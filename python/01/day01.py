f8e0645513d470d4d8ae93026051bb645fd2d9e8
Michael Himing <git@michaelhiming.com>
2020-12-02 09:37:07 +1100

    [python/01] Benchmark loop vs fuctional

--- python/01/day01.py
+++ python/01/day01.py
@@ -1,8 +1,25 @@
 import math
 from itertools import product
 
+from timer import timer
 
-def search(values, n, target=2020):
+
+def loop_solution_1(values, target=2020):
+    for a in values:
+        for b in values:
+            if a + b == target:
+                return a * b
+
+
+def loop_solution_2(values, target=2020):
+    for a in values:
+        for b in values:
+            for c in values:
+                if a + b + c == target:
+                    return a * b * c
+
+
+def functional_solution(values, n, target=2020):
     return next(
         math.prod(v)
         for v in product(values, repeat=n)
@@ -12,12 +29,22 @@ def search(values, n, target=2020):
 def main():
     with open('../../inputs/01.txt') as f:
         values = list(map(int, f))
-    part1 = search(values, 2)
-    print('part1:', part1)
-    assert part1 == 1003971
-    part2 = search(values, 3)
-    print('part2:', part2)
-    assert part2 == 84035952
+
+    part1_solutions = [
+        (loop_solution_1,),
+        (functional_solution, 2),
+    ]
+    for func, *args in part1_solutions:
+        with timer(f'part1 {func.__name__}'):
+            assert func(values, *args) == 1003971
+
+    part2_solutions = [
+        (loop_solution_2,),
+        (functional_solution, 3),
+    ]
+    for func, *args in part2_solutions:
+        with timer(f'part2 {func.__name__}'):
+            assert func(values, *args) == 84035952
 
 
 if __name__ == '__main__':
