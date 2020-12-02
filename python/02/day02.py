05abf8abca1f058969626231688b3c9a184ca928
Michael Himing <git@michaelhiming.com>
2020-12-03 10:26:45 +1100

    [python/02] Init

---
+++ python/02/day02.py
@@ -0,0 +1,29 @@
+import re
+
+
+def parse(line) -> tuple[int, int, str, str]:
+    m = re.fullmatch(r'^(\d+)-(\d+) (\w): (\w+)\n$', line).groups()
+    return int(m[0]), int(m[1]), m[2], m[3]
+
+
+def part1_valid(min_count, max_count, char, password):
+    return min_count <= password.count(char) <= max_count
+
+
+def part2_valid(pos1, pos2, char, password):
+    return (password[pos1 - 1] == char) ^ (password[pos2 - 1] == char)
+
+
+def main():
+    with open('../../inputs/02.txt') as f:
+        data = list(map(parse, f))
+
+    part1 = sum(part1_valid(*args) for args in data)
+    assert part1 == 483
+
+    part2 = sum(part2_valid(*args) for args in data)
+    assert part2 == 482
+
+
+if __name__ == '__main__':
+    main()
