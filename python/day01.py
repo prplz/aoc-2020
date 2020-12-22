25fd854e78b3a684cd13304bca6b75bd63902473
Michael Himing <git@michaelhiming.com>
2020-12-22 16:22:15 +1100

    [python/01] Use combination instead of product...

--- python/day01.py
+++ python/day01.py
@@ -1,28 +1,11 @@
 import math
-from itertools import product
-
-from timer import timer
+from itertools import combinations
 
 
-def loop_solution_1(values, target=2020):
-    for a in values:
-        for b in values:
-            if a + b == target:
-                return a * b
-
-
-def loop_solution_2(values, target=2020):
-    for a in values:
-        for b in values:
-            for c in values:
-                if a + b + c == target:
-                    return a * b * c
-
-
-def functional_solution(values, n, target=2020):
+def solve(values, n, target=2020):
     return next(
         math.prod(v)
-        for v in product(values, repeat=n)
+        for v in combinations(values, n)
         if sum(v) == target)
 
 
@@ -30,21 +13,11 @@ def main():
     with open('../inputs/01.txt') as f:
         values = list(map(int, f))
 
-    part1_solutions = [
-        (loop_solution_1,),
-        (functional_solution, 2),
-    ]
-    for func, *args in part1_solutions:
-        with timer(f'part1 {func.__name__}'):
-            assert func(values, *args) == 1003971
+    part1 = solve(values, 2)
+    assert part1 == 1003971
 
-    part2_solutions = [
-        (loop_solution_2,),
-        (functional_solution, 3),
-    ]
-    for func, *args in part2_solutions:
-        with timer(f'part2 {func.__name__}'):
-            assert func(values, *args) == 84035952
+    part2 = solve(values, 3)
+    assert part2 == 84035952
 
 
 if __name__ == '__main__':
